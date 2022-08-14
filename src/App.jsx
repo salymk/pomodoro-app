b5d430af1e054d698594d058c9a0c31138e5a725
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-08-14 15:22:34 -0600

    Initial app setup

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+
+  return (
+    <div>
+      pomodoro
+      short break
+      long break
+
+      start 
+      pause
+      restart
+
+      Settings
+
+      Time (minutes)
+      pomodoro
+      short break
+      long break
+
+      Font
+      Color
+
+      Apply
+    </div>
+  )
+}
+
+export default App
