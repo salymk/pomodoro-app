abd666afff10ff86e53e643f1f1a7b4e2c9b0725
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-08-22 15:11:58 -0600

    feat: create a theme with colors and fonts from the design

--- theme.js
+++ theme.js
@@ -1,23 +1,42 @@
-import { extendTheme, theme as base, withDefaultColorScheme  } from "@chakra-ui/react"
+import {
+  extendTheme,
+  theme as base,
+  withDefaultColorScheme,
+} from "@chakra-ui/react";
 
 const theme = extendTheme({
-    colors: {
-        brand: {
-            100: 'hsla(0, 0%, 100%, 1)',
-            200: 'hsla(229, 52%, 96%, 1)',
-            300: 'hsla(226, 100%, 92%, 1)',
-            400: 'hsla(0, 91%, 71%, 1)',
-            500: 'hsla(182, 91%, 71%, 1)',
-            600: 'hsla(284, 89%, 74%, 1)',
-            700: 'hsla(235, 35%, 18%, 1)',
-            800: 'hsla(234, 39%, 14%, 1)',
-        }
+  colors: {
+    brand: {
+      100: "hsla(0, 0%, 100%, 1)",
+      200: "hsla(229, 52%, 96%, 1)",
+      300: "hsla(226, 100%, 92%, 1)",
+      400: "hsla(0, 91%, 71%, 1)",
+      500: "hsla(182, 91%, 71%, 1)",
+      600: "hsla(284, 89%, 74%, 1)",
+      700: "hsla(235, 35%, 18%, 1)",
+      800: "hsla(234, 39%, 14%, 1)",
     },
+  },
 
-    fonts: {
-        heading: `Roboto Slab ${base.fonts?.heading}`,
-        body: `Roboto Slab ${base.fonts?.heading}`,
-    },
-})
+  fonts: {
+    heading: `Roboto Slab ${base.fonts?.heading}`,
+    body: `Roboto Slab ${base.fonts?.heading}`,
+  },
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "3.75rem",
+    "7xl": "4.5rem",
+    "8xl": "6.25rem",
+    "9xl": "8rem",
+  },
+});
 
 export default theme;
