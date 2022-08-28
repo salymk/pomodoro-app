89454964bf531736ac18259b15133cac423493dc
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-08-28 12:46:23 -0600

    feat: add model component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import { Heading, Box, Center, VStack, IconButton } from "@chakra-ui/react";
+import {
+  Heading,
+  Box,
+  Center,
+  VStack,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 import TimerTabs from "./components/TimerTabs";
+import SettingsModal from "./components/SettingsModal";
 import { GearIcon } from "./Icons/Icons";
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Center pt="45px" bg="brand.700" minH="100vh" p="24px">
       <Box w="410px">
@@ -22,18 +31,20 @@ function App() {
           </Container> */}
           <Box pt={["79px", "144px", "63px"]}>
             <IconButton
+              onClick={onOpen}
               role="group"
               bg="none"
               _hover={{
                 background: "none",
                 color: "brand.300",
               }}
-              _active="bg: none"
+              _active={{ bg: "none" }}
               color="hsla(226, 100%, 92%, .5)"
               aria-label="Open settings"
               icon={<GearIcon />}
             />
           </Box>
+          <SettingsModal isOpen={isOpen} onClose={onClose} />
         </VStack>
       </Box>
     </Center>
