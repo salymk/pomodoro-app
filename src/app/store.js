2844e084d4b813bcd6e483436710f24d95becd68
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-09-17 15:27:54 -0600

    feat: add color reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import fontSelectorReducer from "../features/font/fontSlice";
+import colorSelectorReducer from "../features/color/colorSlice";
 
 export const store = configureStore({
-  reducer: { fontSelector: fontSelectorReducer },
+  reducer: {
+    fontSelector: fontSelectorReducer,
+    colorSelector: colorSelectorReducer,
+  },
 });
