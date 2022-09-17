55a41cda9754bdc0f75299613e12974f732407f6
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-09-17 11:28:06 -0600

    feat: handle use font selection with RHF

--- src/components/FontGroup.jsx
+++ src/components/FontGroup.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import {
-  Radio,
-  RadioGroup,
   Stack,
-  Heading,
   Flex,
   useRadio,
   useRadioGroup,
-  chakra,
   Box,
-  Image,
+  FormControl,
+  FormLabel,
 } from "@chakra-ui/react";
+import { useController } from "react-hook-form";
 
 function RadioFont(props) {
+  /*
+  useRadio is a custom hook used to provide radio functionality, 
+  as well as state and focus management to custom radio components when using it.
+  */
   const { getInputProps, getCheckboxProps } = useRadio(props);
 
+  // A function to get the input props of the input field
   const input = getInputProps();
+  // A function to get the props of the radio
   const checkbox = getCheckboxProps();
 
   return (
@@ -58,57 +62,65 @@ function RadioFont(props) {
   );
 }
 
-const FontGroup = () => {
-  // const fonts = ["Kumbh Sans", "Roboto Slab", "Space Mono"];
+const FontGroup = ({ control, name }) => {
+  // useController makes it easier to work with uncontrolled components
+  const { field } = useController({
+    name: name,
+    // control contains methods for registering components into RHF
+    control,
+    defaultValue: "Kumbh Sans",
+  });
+
+  // useRadioGroup provides all of the state management logic for a group of radios
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    ...field,
+  });
+
+  // Values for the radio buttons
   const fonts = [
     { font: "Kumbh Sans", weight: 700 },
     { font: "Roboto Slab", weight: 400 },
     { font: "Space Mono", weight: 700 },
   ];
 
-  const { getRootProps, getRadioProps, value } = useRadioGroup({
-    name: "font",
-    defaultValue: "Kumbh Sans",
-    onChange: console.log(),
-  });
-
-  const group = getRootProps();
-
   return (
     <>
-      <Flex
-        flexDirection={["column", "row"]}
-        justifyContent={[null, "space-between"]}
-        alignItems={["center", "baseline"]}
-      >
-        <Heading
-          as="h2"
-          color="brand.800"
-          fontWeight="700"
-          fontSize={["0.6875rem", "0.8125rem"]}
-          lineHeight="14px"
-          letterSpacing="4.2px"
-          textAlign="center"
-          marginBottom="18px"
-          textTransform="uppercase"
+      <FormControl id="font">
+        <Flex
+          flexDirection={["column", "row"]}
+          justifyContent={[null, "space-between"]}
+          alignItems={["center", "baseline"]}
         >
-          Font
-        </Heading>
-        <Stack direction="row" spacing="16px" {...group}>
-          {fonts.map((font) => {
-            return (
-              <RadioFont
-                fontWeight={font.weight}
-                fontFamily={font.font}
-                key={font.font}
-                {...getRadioProps({ value: font.font })}
-              >
-                Aa
-              </RadioFont>
-            );
-          })}
-        </Stack>
-      </Flex>
+          <FormLabel
+            color="brand.800"
+            fontWeight="700"
+            fontSize={["0.6875rem", "0.8125rem"]}
+            lineHeight="14px"
+            letterSpacing="4.2px"
+            textAlign="center"
+            marginBottom="18px"
+            textTransform="uppercase"
+          >
+            Font
+          </FormLabel>
+          {/* getRootProps is a function that takes radio root props and handles changes for the radio group */}
+          <Stack direction="row" spacing="16px" {...getRootProps()}>
+            {fonts.map((font) => {
+              return (
+                <RadioFont
+                  fontWeight={font.weight}
+                  fontFamily={font.font}
+                  key={font.font}
+                  /* getRadioProps is a function that takes root props and handles changes for the radio group */
+                  {...getRadioProps({ value: font.font })}
+                >
+                  Aa
+                </RadioFont>
+              );
+            })}
+          </Stack>
+        </Flex>
+      </FormControl>
     </>
   );
 };
